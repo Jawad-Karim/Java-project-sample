8fafa2dc14a6f2d252a8ec7f587f6b5cee49907e
jawad karim <jawadkarim121@yahoo.com>
2024-08-20 01:40:01 -0400

    added details about Abstraction & Encapsulation.

--- src/oops/abstraction/Animal.java
+++ src/oops/abstraction/Animal.java
@@ -1,5 +1,14 @@
 package oops.abstraction;
 
+/* Abstraction:
+ ----------------------------------------------------------
+Abstraction is a way of hiding unnecessary information. 
+Abstractions solve problems at the design or interface level. 
+The focus of abstraction is “what” to do.
+Abstract classes and interfaces can be used to implement abstractions
+Abstractions allow access to specific pieces of data.
+ */
+
 public abstract class Animal {
 
 	// static final variable
--- src/oops/encapsulation/Student.java
+++ src/oops/encapsulation/Student.java
@@ -1,4 +1,12 @@
 package oops.encapsulation;
+/* Encapsulation:
+-----------------------------------------------------------------
+It is a way of hiding the implementation details by encaptulating data and method as a single entity & unit. 
+Encapsulation solve the problems at implementation level.
+The focus of encapsulation is “how” to do.
+It can be implemented using access modifiers, i.e. private, protected, public. & setter|getter method.
+It hides data from direct access by users (data hiding).
+ */
 
 public class Student {
 	
